15dccce7f4443eb0763adce4da0a35666071b15c
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-02-14 09:33:36 -0500

    Add Functions and Variables

--- Fuctions - Git.py
+++ Fuctions - Git.py
@@ -1,7 +1,7 @@
 #Ethan Blow
 #2.6.19
 #4.13.3: Greeting
-
+'''
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +9,17 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
+'''
+
+#4.13.4: Functions and Variables
+#Ethan Blow
+#2.14.19
+
+x = 2345678954345678954
+
+def print_something():
+    x = 1
+    print (x)
+
+print_something()
+print (x)
