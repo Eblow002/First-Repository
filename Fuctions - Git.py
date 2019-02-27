00ca48211b736a62226959d69577db465fd877da
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-02-27 09:58:21 -0500

    Add functions and variables part 3

--- Fuctions - Git.py
+++ Fuctions - Git.py
@@ -1,7 +1,7 @@
 #Ethan Blow
 #2.6.19
 #4.13.3: Greeting
-'''
+
 name = input("What is your name: ")
 
 def greeting():
@@ -9,7 +9,7 @@ def greeting():
     print("Nice to meet you")
 
 greeting()
-'''
+
 
 #4.13.4: Functions and Variables
 #Ethan Blow
@@ -23,3 +23,28 @@ def print_something():
 
 print_something()
 print (x)
+
+
+#4.13.5: Functions and Variables pt 2
+#Ethan Blow
+#2.14.19
+
+my_variable = 74.7549278567
+
+def something():
+    print(my_variable)
+
+something()
+
+
+
+# 4.13.6: Functions and Variables, Part 3
+# Ethan Blow
+# 2.18.19
+
+
+def print_number(x):
+    print(str(x))
+
+print_number(12)
+print_number('\n' + "Hi")
